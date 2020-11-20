feda1eeae89bee725083a66230308d95c0254d53
Benjamin Eyzaguirre <benjamin@able.co>
2020-11-20 13:55:07 -0500

    Choose version bump type

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const term = require("terminal-kit").terminal;
 const simpleGit = require("simple-git");
 const git = simpleGit();
+const inc = require("semver/functions/inc");
 const { computeLatest } = require("./utils");
 
 (async () => {
@@ -17,6 +18,27 @@ const { computeLatest } = require("./utils");
 
   const latest = computeLatest(await git.tags());
   term("Latest version detected: ").green("%s\n", latest);
+  term("What kind of release is this?");
+
+  const patchTypes = ["patch", "minor", "major"];
+  const optionsText = patchTypes.map(
+    (patch) => `${patch} ${inc(latest, patch)}`
+  );
+  prompt = term.singleColumnMenu(optionsText);
+  result = await prompt.promise;
+
+  const toBeVersion = `v${inc(latest, patchTypes[result.selectedIndex])}`;
+
+  term("This will bump ").yellow(latest)(" to ").green(toBeVersion)(
+    "\nAre you sure you want to push this? [y|N]\n"
+  );
+  prompt = term.yesOrNo({ yes: ["y", "Y"], no: ["n", "N", "ENTER"] });
+  result = await prompt.promise;
+  if (!result) {
+    term.red("'No' detected. Good bye!\n");
+    process.exit();
+  }
+  term.green("All done.\n");
 
   process.exit();
 })();
