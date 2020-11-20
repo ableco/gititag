df63bc9479d5d5b13ba281e2181effa65c77d5df
Benjamin Eyzaguirre <benjamin@able.co>
2020-11-20 11:44:49 -0500

    Get current branch name

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+const term = require("terminal-kit").terminal;
+const simpleGit = require("simple-git");
+const git = simpleGit();
+
+(async () => {
+  const { current: branch } = await git.branch();
+  term("Welcome to git interactive tag!\n");
+  term("Your current branch is: ").green("%s\n", branch);
+})();
