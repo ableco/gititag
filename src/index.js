c0b8db0b261b964f23d1b258332a81f8ca745d03
Benjamin Eyzaguirre <benjamin@able.co>
2020-11-20 11:51:07 -0500

    Add initial prompt

--- src/index.js
+++ src/index.js
@@ -6,4 +6,14 @@ const git = simpleGit();
   const { current: branch } = await git.branch();
   term("Welcome to git interactive tag!\n");
   term("Your current branch is: ").green("%s\n", branch);
+  term("Do you want to tag/release this? [y|N]\n");
+  let prompt = term.yesOrNo({ yes: ["y", "Y"], no: ["n", "N", "ENTER"] });
+  let result = await prompt.promise;
+  if (!result) {
+    term.red("'No' detected. Good bye!\n");
+    process.exit();
+  }
+
+  term.green("'Yes' detected! See ya soon!\n");
+  process.exit();
 })();
